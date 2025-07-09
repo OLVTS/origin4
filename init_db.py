256c1ee9c497ce3ab9ede595a74722ed778966f2
OLVTS <cojoleg89@gmail.com>
2025-07-09 18:16:32 +0500

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,15 +1,13 @@
 import asyncio
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
+from db_base import Base
+from config import DATABASE_URL
+import models  # обязательно импортируй модели!
 
-from db_base import Base  # Здесь находится Base = declarative_base()
-from config import DATABASE_URL  # Убедись, что этот URL подключается к PostgreSQL
-
-# Создание движка
 engine: AsyncEngine = create_async_engine(DATABASE_URL, echo=True)
 
 async def init_db():
     async with engine.begin() as conn:
-        # Создание всех таблиц на основе моделей, зарегистрированных в Base
         await conn.run_sync(Base.metadata.create_all)
     print("✅ Таблицы успешно созданы.")
 
