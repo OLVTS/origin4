ce9bad3fba9af2523029ef25457fa3ba14ce3af3
OLVTS <cojoleg89@gmail.com>
2025-07-15 19:59:47 +0500

    Update states.py

--- states.py
+++ states.py
@@ -2,12 +2,11 @@ from aiogram.fsm.state import State, StatesGroup
 
 class AddProperty(StatesGroup):
     location = State()
+    media = State()
     description = State()
     condition = State()
     parking = State()
     bathrooms = State()
     additions = State()
     price = State()
-    title = State()
-    media = State()
     confirm = State()
